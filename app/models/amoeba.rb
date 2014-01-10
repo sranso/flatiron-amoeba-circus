bbebc9cd700c7ba064656cbde4d7b1e3d6374467
sranso <sranso@gmail.com>
2014-01-10 10:31:00 -0500

    split works

--- app/models/amoeba.rb
+++ app/models/amoeba.rb
@@ -3,13 +3,10 @@ class Amoeba < ActiveRecord::Base
   belongs_to :act
   has_one :talent
 
-  # def initialize
-  #   @gen ||= 1
-  # end
-
   def split
-    # first = 
-    # second = 
-    # destroy
+    consonants = ["b","c","d","f","g","h","j","ch","gh","m","qu"]
+    first = Amoeba.create(:name => name.gsub(/[^aeiou]/, consonants.sample), :act_id => act_id, :talent_id => talent_id, :generation_num => generation_num + 1)
+    second = Amoeba.create(:name => name.gsub(/[^aeiou]/, consonants.sample), :act_id => act_id, :talent_id => talent_id, :generation_num => generation_num + 1)
+    self.destroy
   end
 end
