5a449365a88830613f6fab1826a9d9ba52ad081c
sranso <sranso@gmail.com>
2014-01-10 13:27:14 -0500

    seed database, show act and talent names in /amoebas

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,25 @@
 #
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
+
+Talent.create([{name: "Acrobat"},
+  {name: "Contortionist"},
+  {name: "Clown"},
+  {name: "Juggler"},
+  {name: "Freak"}])
+
+Act.create([{name: "The Great Escape", date_time: "2014/12/31 11:59pm"}, 
+  {name: "Feelings Friday", date_time: "2014/11/01 4pm"}, 
+  {name: "Taking Toys", date_time: "2016/10/12 8pm"}])
+
+Amoeba.create([{name: "Floo", act_id: 1, talent_id: 2},
+  {name: "Floo", act_id: 1, talent_id: 1},
+  {name: "Floo", act_id: 2, talent_id: 3},
+  {name: "Floo", act_id: 2, talent_id: 4},
+  {name: "Floo", act_id: 2, talent_id: 5},
+  {name: "Floo", act_id: 3, talent_id: 5},
+  {name: "Floo", act_id: 3, talent_id: 4},
+  {name: "Floo", act_id: 3, talent_id: 3},
+  {name: "Floo", act_id: 3, talent_id: 2},
+  {name: "Floo", act_id: 3, talent_id: 1},
+  {name: "Floo", act_id: 1, talent_id: 1}])
