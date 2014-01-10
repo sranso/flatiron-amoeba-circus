294dbf2766418248cdabc37e9e74d224f33fadda
sranso <sranso@gmail.com>
2014-01-10 12:43:02 -0500

    create talents

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20140110172912) do
+ActiveRecord::Schema.define(:version => 20140110173909) do
 
   create_table "acts", :force => true do |t|
     t.string   "name"
@@ -27,6 +27,12 @@ ActiveRecord::Schema.define(:version => 20140110172912) do
     t.integer  "talent_id"
     t.datetime "created_at",                :null => false
     t.datetime "updated_at",                :null => false
+  end
+
+  create_table "talents", :force => true do |t|
+    t.string   "name"
+    t.datetime "created_at", :null => false
+    t.datetime "updated_at", :null => false
   end
 
 end
